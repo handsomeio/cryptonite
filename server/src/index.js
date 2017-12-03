23fb83679c469de6fe6c82ad32b05b333abfd2da
szlezak <robertszlezak@gmail.com>
2017-12-03 20:04:58 +0100

    Created webinar form

--- server/src/index.js
+++ server/src/index.js
@@ -58,6 +58,28 @@ app.post('/subscribe', jsonParser, async (req, res) => {
   }
 });
 
+
+app.post('/webinar/subscribe', jsonParser, async (req, res) => {
+  const { email, firstName, lastName } = req.body;
+  axios({
+    method: "post",
+    url,
+    data: {
+      "email_address": email,
+      "status": "subscribed",
+    },
+    headers: {
+      "Access-Control-Allow-Credentials":"true",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    auth,
+  }).then((response) => {
+    res.json(response.data);
+  }).catch((error) => {
+    res.status(500).json({ error })
+  });
+});
+
 app.listen(4000, () => {
   console.log('Bitcoach app listening on port 4000!')
 });
